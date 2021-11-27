7e538a4eb2587b293fbdb61b6cba47740553879a
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-11-27 21:00:14 +0100

    feat: 🎸 add league client credential reader

--- packages/client/src/domain/shell/views/Shell/index.tsx
+++ packages/client/src/domain/shell/views/Shell/index.tsx
@@ -3,10 +3,21 @@ import { useEffect } from 'react';
 import { TitleBar } from '../../components';
 import { invoke } from '@tauri-apps/api/tauri';
 import * as C from './styles';
+import { LCUCredentials } from '@universe/client/types';
 
 export const Shell: FC = ({ children }) => {
   useEffect(() => {
     invoke('close_splashscreen');
+
+    const interval = setInterval(() => {
+      invoke<LCUCredentials | {}>('get_league_credentials').then(
+        (credentials) => {
+          console.log(credentials);
+        },
+      );
+    }, 2500);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
---
+++ packages/client/src/types/lcu.types.ts
@@ -0,0 +1,9 @@
+export interface LCUCredentials {
+  username: string;
+  process: string;
+  address: string;
+  PID: number;
+  port: number;
+  password: string;
+  protocol: string;
+}
